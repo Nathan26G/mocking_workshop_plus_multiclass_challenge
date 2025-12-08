9cb110cb20926f7795dc2f82d9bd8da208f150e5
Nathan Gallagher <nathangallagher101@gmail.com>
2025-12-08 13:55:44 +0000

    test drove student class

--- lib/student.py
+++ lib/student.py
@@ -1,2 +1,8 @@
 class Student:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.list_of_submissions = []
+    def add_submission(self, submission):
+        self.list_of_submissions.append(submission)
+    def count_submissions(self):
+        return len(self.list_of_submissions)
--- tests/test_student.py
+++ tests/test_student.py
@@ -1,2 +1,43 @@
 from lib.student import Student
 
+def test_return_empty_list_after_being_initialised():
+    student = Student('Nathan')
+    submission_list = student.list_of_submissions
+    
+    assert submission_list == []
+
+def test_return_list_after_adding_a_submission():
+    student = Student('Nathan')
+    student.add_submission('submission1')
+    submission_list = student.list_of_submissions
+    
+    assert submission_list == ['submission1']
+
+def test_return_list_after_adding_multiple_submissios():
+    student = Student('Nathan')
+    student.add_submission('submission1')
+    student.add_submission('submission2')
+    submission_list = student.list_of_submissions
+    
+    assert submission_list == ['submission1', 'submission2']
+
+def test_return_count_before_submissions():
+    student = Student('Nathan')
+    count = student.count_submissions()
+    
+    assert count == 0
+
+def test_return_count_after_adding_a_submission():
+    student = Student('Nathan')
+    student.add_submission('submission1')
+    count = student.count_submissions()
+    
+    assert count == 1
+    
+def test_return_count_after_adding_multiple_submissios():
+    student = Student('Nathan')
+    student.add_submission('submission1')
+    student.add_submission('submission2')
+    count = student.count_submissions()
+    
+    assert count == 2
